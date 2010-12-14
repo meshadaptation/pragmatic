5376ffc9cd046a1fd4cd5b7e70ee1862dac24c10
ggorman <>
2010-12-14 21:22:50 +0000

    First draft of metric tensor classes

---
+++ metric_tensor/include/MetricField.h
@@ -0,0 +1,325 @@
+/*
+ *    Copyright (C) 2010 Imperial College London and others.
+ *    
+ *    Please see the AUTHORS file in the main source directory for a full list
+ *    of copyright holders.
+ *
+ *    Gerard Gorman
+ *    Applied Modelling and Computation Group
+ *    Department of Earth Science and Engineering
+ *    Imperial College London
+ *
+ *    amcgsoftware@imperial.ac.uk
+ *    
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation,
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ *    USA
+ */
+
+#ifndef METRICFIELD_H
+#define METRICFIELD_H
+
+#include <iostream>
+
+#include <Eigen/Core>
+#include <Eigen/Dense>
+
+#ifdef HAVE_MPI
+#include <mpi.h>
+#endif
+
+/*! \brief Constructs metric tensor field which encodes anisotropic
+ *  edge size information.
+ * 
+ * Use this class to create a metric tensor field using desired error
+ * tolerences and curvature of linear fields subject to a set of constraints.
+ * index_t should be set as int32_t for problems with less than 2 billion nodes.
+ * For larger meshes int64_t should be used.
+ */
+template<typename real_t, typename index_t>
+  class MetricField{
+ public:
+  
+  /// Default constructor.
+  MetricField(){
+    _NNodes = 0;
+    _NElements = 0;
+    _ndims = 0;
+    _ENList = NULL;
+    _node_distribution = NULL;
+    _x = NULL;
+    _y = NULL;
+    _z = NULL;
+#ifdef HAVE_MPI
+    _mesh_comm = NULL;
+#endif
+    _metric = NULL;
+  }
+
+  /// Default destructor.
+  ~MetricField(){
+    if(metric!=NULL)
+      delete [] metric;
+  }
+  
+  /*! Set the source mesh (2D triangular meshes).
+   * @param NNodes number of nodes in the local mesh.
+   * @param NElements number of nodes in the local mesh.
+   * @param ENList array storing the global node number for each element.
+   * @param x is the X coordinate.
+   * @param y is the Y coordinate.
+   * @param node_distribution gives the number of nodes owned
+   *        by each process when this is using MPI, where it is
+   *        assumed that each processes ownes n_i consecutive
+   *        vertices of the mesh. Its contents are identical for every process.
+   */
+  void set_mesh(int NNodes, int NElements, const index_t *ENList,
+                const real_t *x, const real_t *y, const index_t *node_distribution=NULL){
+    _ndim = 2;
+    _NNodes = NNodes;
+    _NElements = NElements;
+    _ENList = ENList;
+    _x = x;
+    _y = y;
+    _z = NULL;
+    _node_distribution = node_distribution;
+  }
+  
+  /*! Set the source mesh (3D tetrahedral meshes).
+   * @param NNodes number of nodes in the local mesh.
+   * @param NElements number of nodes in the local mesh.
+   * @param ENList array storing the global node number for each element.
+   * @param x is the X coordinate.
+   * @param y is the Y coordinate.
+   * @param z is the Z coordinate.
+   * @param node_distribution gives the number of nodes owned
+   *        by each process when this is using MPI, where it is
+   *        assumed that each processes ownes n_i consecutive
+   *        vertices of the mesh. Its contents are identical for every process.
+   */
+  void set_mesh(int NNodes, int NElements, const index_t *ENList,
+                const real_t *x, const real_t *y, const real_t *z, const index_t *node_distribution=NULL){
+    _ndim = 3;
+    _NNodes = NNodes;
+    _NElements = NElements;
+    _ENList = ENList;
+    _x = x;
+    _y = y;
+    _z = z;
+    _node_distribution = node_distribution;
+  }
+  
+#ifdef HAVE_MPI
+  /*! Set the MPI communicator to be used.
+   * @param mesh_comm is the MPI communicator.
+   */
+  void set_mpi_communicator(const MPI_Comm *mesh_comm){
+    _mesh_comm = mesh_comm;
+  }
+#endif
+
+  /*! Add the contribution from the metric field from a new field with a target linear interpolation error.
+   * @param psi is field while curvature is to be considered.
+   * @param target_error is the target interpolation error.
+   * @param relative is true if interpolation error is relative, otherwise it is assumed to be an absolute error.
+   * @param sigma_psi should be set if a relative interpolation error is specified. It is the minimum value for psi used when applying the relative error.
+   */
+  void add_field(const real_t *psi, const real_t target_error, bool relative, real_t sigma_psi){
+    int ndims2 = _ndims*_ndims;
+    
+    real_t *Hessian = new real_t[_NNodes*ndims2];
+    
+    get_hessian(psi, Hessian);
+    
+    if(relative){
+      for(size_t i=0;i<_NNodes;i++){
+        real_t eta = 1.0/max(target_error*psi[i], sigma_psi);
+        for(size_t j=0;j<ndims2;j++)
+          Hessian[i*ndims2+j]*=eta;
+      }
+    }else{
+      for(size_t i=0;i<_NNodes;i++){
+        real_t eta = 1.0/target_error;
+        for(size_t j=0;j<ndims2;j++)
+          Hessian[i*ndims2+j]*=eta;
+      }
+    }
+    
+    if(metric==NULL){
+      _metric = new MetricTensor<real_t>[_NNodes];
+      for(size_t i=0;i<_NNodes;i++)
+        _metric[i].set(_ndims, Hessian+i*ndims2);
+    }else{
+      for(size_t i=0;i<_NNodes;i++)
+        _metric[i].constrain(MetricTensor<real_t>(ndims, Hessian+i*ndims2));
+    }
+    delete [] Hessian;
+  }
+  
+  /*! Apply maximum edge length constraint.
+   */
+  void apply_max_edge_length(real_t max_len){
+    real_t M[_ndims*_ndims];
+    for(size_t i=0;i<_ndims;i++)
+      for(size_t j=0;j<_ndims;j++)
+        if(i==j)
+          M[i*_ndims+j] = 1.0/(max_len*max_len);
+        else
+          M[i*_ndims+j] = 0.0;
+    
+    MetricTensor<real_t> constraint(_ndims, M);
+    
+    for(size_t i=0;i<_NNodes;i++)
+      _metric[i].constrain(constraint);
+  }
+  
+  /*! Apply minimum edge length constraint.
+   */
+  void apply_min_edge_length(real_t min_len){
+    real_t M[_ndims*_ndims];
+    for(size_t i=0;i<_ndims;i++)
+      for(size_t j=0;j<_ndims;j++)
+        if(i==j)
+          M[i*_ndims+j] = 1.0/(max_len*max_len);
+        else
+          M[i*_ndims+j] = 0.0;
+    
+    MetricTensor<real_t> constraint(_ndims, M);
+    
+    for(size_t i=0;i<_NNodes;i++)
+      _metric[i].constrain(constraint, false);
+  }
+  
+  /*! Apply maximum aspect ratio constraint.
+   */
+  void apply_max_aspect_ratio(real_t max_aspect_ratio);
+
+  /*! Apply maximum number of elements constraint.
+   */
+  void apply_max_nelements(real_t max_nelements);
+
+  /*! Apply minimum number of elements constraint.
+   */
+  void apply_min_nelements(real_t min_nelements);
+
+  /*! Apply required number of elements.
+   */
+  void apply_nelements(real_t nelements);
+
+ private:
+  /*! Apply required number of elements.
+   */
+  void get_hessian(const real_t *psi, real_t *Hessian){
+    
+    // Create node-node list
+    if(NNList.empty()){
+      NNList.resize(_NNodes);
+      size_t nloc = (_ndims==2)?3:4;
+      for(size_t i=0; i<_NElements; i++){
+        for(size_t j=0;j<nloc;j++){
+          for(size_t k=j+1;k<nloc;k++){
+            NNlist[_ENList[i*nloc+j]].insert(_ENList[i*nloc+k]);
+            NNlist[_ENList[i*nloc+k]].insert(_ENList[i*nloc+j]);
+          }
+        }
+      }
+    }
+    
+    // Calculate Hessian at each point.
+    int min_patch_size = (ndims==2)?6:10;
+    for(size_t i=0; i<_NNodes; i++){
+      set<size_t> patch = NNList[i];
+      
+      if(patch.size()<min_patch_size){
+        set<size_t> front = NNList[i];
+        set<size_t> new_front;
+        while(patch.size()<min_patch_size){
+          for(set<size_t>::const_iterator it=front.begin();it!=front.end();it++){
+            if(patch.count(*it)==0){
+              new_front.insert(*it);
+              patch.insert(*it);
+            }
+          }
+          if(new_front.empty()){
+            cerr<<"WARNING: Small mesh patch detected.\n";  
+            break;
+          }
+          front.swap(new_front);
+          new_front.clear();
+        }
+      }
+      
+      if(ndims==2){
+        // Form quadratic system to be solved. The quadratic fit is:
+        // P = 1 + x + y + x^2 + xy + y^2
+        // A = P^TP
+        real_t x=_x[i], y=_y[i], z=_z[i];
+        MatrixXf A = MatrixXf::Zero(6,6);
+        
+        A[0]+=1; A[1]+=x; A[2]+=y; A[3]+=pow(x,2); A[4]+=x*y; A[5]+=pow(y,2);
+        A[6]+=x; A[7]+=pow(x,2); A[8]+=x*y; A[9]+=pow(x,3); A[10]+=pow(x,2)*y; A[11]+=x*pow(y,2);
+        A[12]+=y; A[13]+=x*y; A[14]+=pow(y,2); A[15]+=pow(x,2)*y; A[16]+=x*pow(y,2); A[17]+=pow(y,3);
+        A[18]+=pow(x,2); A[19]+=pow(x,3); A[20]+=pow(x,2)*y; A[21]+=pow(x,4); A[22]+=pow(x,3)*y; A[23]+=pow(x,2)*pow(y,2);
+        A[24]+=x*y; A[25]+=pow(x,2)*y; A[26]+=x*pow(y,2); A[27]+=pow(x,3)*y; A[28]+=pow(x,2)*pow(y,2); A[29]+=x*pow(y,3);
+        A[30]+=pow(y,2); A[31]+=x*pow(y,2); A[32]+=pow(y,3); A[33]+=pow(x,2)*pow(y,2); A[34]+=x*pow(y,3); A[35]+=pow(y,4);
+
+        VectorXf b = VectorXf::Zero(6);
+        b[0]+=psi[i]*1;
+        b[1]+=psi[i]*x;
+        b[2]+=psi[i]*y;
+        b[3]+=psi[i]*pow(x,2);
+        b[4]+=psi[i]*x*y;
+        b[5]+=psi[i]*pow(y,2);
+
+        for(set<size_t>::const_iterator n=patch.begin(); n!=patch.end(); n++){
+          x=_x[*n], y=_y[*n], z=_z[*n];
+          
+          A[0]+=1; A[1]+=x; A[2]+=y; A[3]+=pow(x,2); A[4]+=x*y; A[5]+=pow(y,2);
+          A[6]+=x; A[7]+=pow(x,2); A[8]+=x*y; A[9]+=pow(x,3); A[10]+=pow(x,2)*y; A[11]+=x*pow(y,2);
+          A[12]+=y; A[13]+=x*y; A[14]+=pow(y,2); A[15]+=pow(x,2)*y; A[16]+=x*pow(y,2); A[17]+=pow(y,3);
+          A[18]+=pow(x,2); A[19]+=pow(x,3); A[20]+=pow(x,2)*y; A[21]+=pow(x,4); A[22]+=pow(x,3)*y; A[23]+=pow(x,2)*pow(y,2);
+          A[24]+=x*y; A[25]+=pow(x,2)*y; A[26]+=x*pow(y,2); A[27]+=pow(x,3)*y; A[28]+=pow(x,2)*pow(y,2); A[29]+=x*pow(y,3);
+          A[30]+=pow(y,2); A[31]+=x*pow(y,2); A[32]+=pow(y,3); A[33]+=pow(x,2)*pow(y,2); A[34]+=x*pow(y,3); A[35]+=pow(y,4);
+          
+          VectorXf b = VectorXf::Zero(6);
+          b[0]+=psi[*n]*1;
+          b[1]+=psi[*n]*x;
+          b[2]+=psi[*n]*y;
+          b[3]+=psi[*n]*pow(x,2);
+          b[4]+=psi[*n]*x*y;
+          b[5]+=psi[*n]*pow(y,2);
+        }
+        
+        VectorXf x;
+        A.lu().solve(b, &x);
+        
+        Hessian[i*4  ] = b[3]*2.0; // d2/dx2
+        Hessian[i*4+1] = b[4];     // d3/dxdy
+        Hessian[i*4+2] = b[4];     // d3/dxdy
+        Hessian[i*4+3] = b[5]*2.0; // d2/dy2
+      }else{
+        std::cerr<<"ERROR: 3D not implemented yet\n";
+      }
+    }
+  }
+  
+  int _NNodes, _NElements, _ndims;
+  const index_t *_ENList, *_node_distribution;
+  const real_t *_x, *_y, *_z;
+  std::vector< std::set<index_t> > NNList;
+#ifdef HAVE_MPI
+  const MPI_Comm *_mesh_comm;
+#endif
+  MetricTensor<real_t> *_metric;
+};
---
+++ metric_tensor/include/MetricTensor.h
@@ -0,0 +1,262 @@
+/*
+ *    Copyright (C) 2010 Imperial College London and others.
+ *    
+ *    Please see the AUTHORS file in the main source directory for a full list
+ *    of copyright holders.
+ *
+ *    Gerard Gorman
+ *    Applied Modelling and Computation Group
+ *    Department of Earth Science and Engineering
+ *    Imperial College London
+ *
+ *    amcgsoftware@imperial.ac.uk
+ *    
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation,
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ *    USA
+ */
+#ifndef METRICFIELD_H
+#define METRICFIELD_H
+
+#include <iostream>
+
+#include <Eigen/Core>
+#include <Eigen/Dense>
+
+template<typename real_t>
+class MetricTensor;
+
+template<typename real_t>
+std::ostream& operator<<(std::ostream& out, const MetricTensor<real_t>& in);
+
+/*! \brief Symmetric metic tensor class.
+ * 
+ * Use to store and operate on metric tensors.
+ */
+template<typename real_t>
+class MetricTensor{
+ public:
+  /*! Empty constructor.
+   */
+  MetricTensor(){
+    _dimension = 0;
+    _metric = NULL;
+  }
+  
+  /*! Constructor.
+   * @param dimension of tensor.
+   * @param metric points to the dimension*dimension tensor.
+   */
+  MetricTensor(int dimension, const real_t *metric){
+    set(dimension, metric);
+  }
+
+  /*! Copy constructor.
+   * @param metric is a reference to a MetricTensor object.
+   */
+  MetricTensor(const MetricTensor& metric){
+    *this = metric;
+  }
+
+  /*! Assignment operator.
+   * @param metric is a reference to a MetricTensor object.
+   */
+  const MetricTensor& operator=(const MetricTensor &metric){
+    _dimension = metric._dimension;
+    if(_metric==NULL)
+      _metric = new real_t[_dimension*_dimension];
+    for(size_t i=0;i<_dimension*_dimension;i++)
+      _metric[i] = metric._metric[i];
+    return *this;
+  }
+  
+  /*! By default this calculates the superposition of two metrics where by default small
+   * edge lengths are preserved. If the optional argument perserved_small_edges==false
+   * then large edge lengths are perserved instead.
+   * @param metric is a reference to a MetricTensor object.
+   * @param perserved_small_edges when true causes small edge lengths to be preserved (default). Otherwise long edge are perserved.
+   */
+  void constrain(const MetricTensor &metric, bool perserved_small_edges=true){
+    assert(_dimension==metric._dimension);
+    
+    // Make the tensor with the smallest aspect ratio the reference space Mr.
+    const double *Mr=_metric, *Mi=metric._metric;
+    if(_dimension==2){
+      Matrix<real_t, 2, 2> M1;
+      M1 <<
+        _metric[0], _metric[1],
+        _metric[2], _metric[3];
+      
+      Eigen::EigenSolver< Eigen::Matrix<double, 2, 2> > solver(M1);
+
+      Eigen::Matrix<double, 2, 1> evalues1 = solver.eigenvalues().real();
+      
+      double aspect_r = std::min(evalues1[0], evalues1[1]);
+
+      Matrix<real_t, 2, 2> M2;
+      M2 <<
+        metric._metric[0], metric._metric[1],
+        metric._metric[2], metric._metric[3];
+      
+      Eigen::EigenSolver< Eigen::Matrix<double, 2, 2> > solver(M2);
+
+      Eigen::Matrix<double, 2, 1> evalues2 = solver.eigenvalues().real();
+      
+      double aspect_i = std::min(evalues2[0], evalues2[1]);
+      
+      if(aspect_i>aspect_r){
+        Mi=_metric;
+        Mr=metric._metric;
+      }
+    }else{
+      Matrix<real_t, 3, 3> M1;
+      M1 <<
+        _metric[0], _metric[1], _metric[2],
+        _metric[3], _metric[4], _metric[5],
+        _metric[6], _metric[7], _metric[8];
+      
+      Eigen::EigenSolver< Eigen::Matrix<double, 3, 3> > solver(M1);
+      
+      Eigen::Matrix<double, 3, 1> evalues1 = solver.eigenvalues().real();
+      
+      double aspect_r = std::min(std::min(evalues1[0], evalues1[1]), evalues1[2])/
+        std::max(std::max(evalues1[0], evalues1[1]), evalues1[2]);
+
+      Matrix<real_t, 3, 3> M2;
+      M2 <<
+        metric._metric[0], metric._metric[1], metric._metric[2],
+        metric._metric[3], metric._metric[4], metric._metric[5],
+        metric._metric[6], metric._metric[7], metric._metric[8];
+      
+      Eigen::EigenSolver< Eigen::Matrix<double, 3, 3> > solver(M2);
+      
+      Eigen::Matrix<double, 3, 1> evalues2 = solver.eigenvalues().real();
+      
+      double aspect_i = std::min(std::min(evalues2[0], evalues2[1]), evalues2[2])/
+        std::max(std::max(evalues2[0], evalues2[1]), evalues2[2]);
+      
+      if(aspect_i>aspect_r){
+        Mi=metric;
+        Mr=metric._metric;
+      }
+    }
+    
+    // Map Mi to the reference space where Mr==I
+    if(dimension==2){
+      Matrix<real_t, 2, 2> M1;
+      M1 <<
+        Mr[0], Mr[1],
+        Mr[2], Mr[3];
+      
+      Eigen::EigenSolver< Eigen::Matrix<double, 2, 2> > solver(M1);
+
+      Eigen::Matrix<double, 2, 2> F =
+        solver.eigenvalues().real().cwise().abs().cwise().sqrt().asDiagonal()*
+        solver.eigenvectors().real();
+      
+      Matrix<real_t, 2, 2> M2;
+      M2 <<
+        Mi[0], Mi[1],
+        Mi[2], Mi[3];
+      Eigen::Matrix<double, 2, 2> M = F.inverse().transpose()*M2*F.inverse();
+
+      Eigen::EigenSolver< Eigen::Matrix<double, 2, 2> > solver2(M);      
+      Eigen::Matrix<double, 2, 1> evalues = solver2.eigenvalues().real();
+      Eigen::Matrix<double, 2, 2> evectors = solver2.eigenvectors().real();
+      if(perserved_small_edges)
+        for(size_t i=0;i<2;i++)
+          evalues[i] = std::max((double)1.0, fabs(evalues[i]));
+      else
+        for(size_t i=0;i<2;i++)
+          evalues[i] = std::min((double)1.0, fabs(evalues[i]));
+
+      Matrix<real_t, 2, 2> Mc = eigenvectors.transpose()*evalues.asDiagonal()*eigenvectors;
+      Mc = F.transpose()*Mc*F;
+      
+      for(size_t i=0;i<dimension*dimension;i++)
+        _metric[i] = Mc[i];
+    }else{
+      Matrix<real_t, 3, 3> M1;
+      M1 <<
+        Mr[0], Mr[1], Mr[2],
+        Mr[3], Mr[4], Mr[5],
+        Mr[6], Mr[7], Mr[8];
+      
+      Eigen::EigenSolver< Eigen::Matrix<double, 3, 3> > solver(M1);
+      
+      Eigen::Matrix<double, 3, 3> F =
+        solver.eigenvalues().real().cwise().abs().cwise().sqrt().asDiagonal()*
+        solver.eigenvectors().real();
+      
+      Matrix<real_t, 3, 3> M2;
+      M2 <<
+        Mi[0], Mi[1], Mi[2],
+        Mi[3], Mi[4], Mi[5],
+        Mi[6], Mi[7], Mi[8];
+      Eigen::Matrix<double, 3, 3> M = F.inverse().transpose()*M2*F.inverse();
+        
+      Eigen::EigenSolver< Eigen::Matrix<double, 3, 3> > solver2(M);      
+      Eigen::Matrix<double, 3, 1> evalues = solver2.eigenvalues().real();
+      Eigen::Matrix<double, 3, 3> evectors = solver2.eigenvectors().real();
+
+      if(perserved_small_edges)
+        for(size_t i=0;i<3;i++)
+          evalues[i] = std::max((double)1.0, fabs(evalues[i]));
+      else
+        for(size_t i=0;i<3;i++)
+          evalues[i] = std::min((double)1.0, fabs(evalues[i]));
+
+      Matrix<real_t, 3, 3> Mc = eigenvectors.transpose()*evalues.asDiagonal()*eigenvectors;
+      Mc = F.transpose()*Mc*F;
+
+      for(size_t i=0;i<dimension*dimension;i++)
+        _metric[i] = Mc[i];
+    }
+    
+    return 0; 
+  }
+  
+  /*! Stream operator.
+   */
+  friend std::ostream &operator<< <>(std::ostream& out, const MetricTensor<real_t>& metric);
+
+  /*! Set values from input.
+   * @param dimension of tensor.
+   * @param metric points to the dimension*dimension tensor.
+   */
+  void set(int dimension, const real_t *metric){
+    _dimension = dimension;
+    metric = new real_t[_dimension*_dimension];
+    for(size_t i=0;i<_dimension*_dimension;i++)
+      _metric[i] = metric[i];
+  }
+  
+ private:
+  real_t *_metric;
+  size_t _dimension;
+};
+
+template<typename real_t>
+std::ostream &operator<<(std::ostream& out, const MetricTensor<real_t>& in){
+  const real_t *g = in.metric;
+  for(size_t i=0; i<dimension; i++){
+    for(size_t j=0; j<dimension; j++){
+      out<<*g<<" "; g++;
+    }
+    out<<"\n";
+  }
+  return out;
+}
+
+#endif
