b4ed3017ac4da370bccabfd087e99249eea70ff6
ggorman <>
2011-01-14 16:17:11 +0000

    bug fix

--- tests/benchmark.sh
+++ tests/benchmark.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-benchmark $1
-input = $2
-ntrials = $3
-max_threads = $4
+benchmark=$1
+input=$2
+ntrials=$3
+max_threads=$4
 
-output = $(benchmark).log
-cat /dev/null > $(output)
+output=${benchmark}.log
+cat /dev/null > ${output}
 
 for((n=0;n<ntrials;n++))
 do
     for((i=1;i<=max_threads;i++))
     do
         echo "Running trial $n on $i threads."
-        OMP_NUM_THREADS=$i $(benchmark) $(input) 2>&1 >> $(output)
+	echo "OMP_NUM_THREADS=$i ${benchmark} ${input} 2>&1 >> ${output}"
+        OMP_NUM_THREADS=$i ${benchmark} ${input} 2>&1 >> ${output}
     done
 done
