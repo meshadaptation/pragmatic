47d843276d92a02819ec514ef087a0a3c06a123b
ggorman <>
2011-01-14 16:07:31 +0000

    bugfix

--- tests/benchmark.sh
+++ tests/benchmark.sh
@@ -8,10 +8,11 @@ max_threads = $4
 output = $(benchmark).log
 cat /dev/null > $(output)
 
-for((n=0;n<$(ntrials);n++))
+for((n=0;n<ntrials;n++))
 do
-    for((i=1;i<=$(max_threads);i++))
+    for((i=1;i<=max_threads;i++))
     do
+        echo "Running trial $n on $i threads."
         OMP_NUM_THREADS=$i $(benchmark) $(input) 2>&1 >> $(output)
     done
 done
