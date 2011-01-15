b28a46f93bb8d65c0c1367127e6a8177ee962bc8
ggorman <>
2011-01-15 11:45:17 +0000

    Support for multiple input logs from experiments.

--- tests/benchmark-analysis.py
+++ tests/benchmark-analysis.py
@@ -7,62 +7,82 @@ import sys
 import getopt
 
 # Defaults
-file = sys.stdin
+files = []
 column = 0
 ntrials = 1
-benchmark="Benchmark"
+key=""
 
-opts, args = getopt.getopt(sys.argv[1:], 'c:t:b:')
+opts, args = getopt.getopt(sys.argv[1:], 'p:t:b:')
 
 for o, a in opts:
-    if o == '-c':
-        column = int(a)
+    if o == '-p':
+        key, ocol = a.split(":")
+        column = int(ocol)
     elif o == '-t':
         ntrials = int(a)
     elif o == '-b':
         benchmark = a
 
-if len(args)>0:
-    file = open(args[0], "r")
+if len(args)==0:
+    files = [sys.stdin]
+else:
+    for file in args:
+        files = files + [(open(file, "r"), file[:-4])]
 
-times = array([float(line.split()[column]) for line in file])
-nthreads = len(times)/ntrials
+for file, filename in files:
+    t = []
+    for line in file:
+        if line.count(key):
+            t = t + [float(line.split()[column])]
+    times = array(t)
+    nthreads = len(times)/ntrials
 
-times.shape = (ntrials, nthreads)
+    times.shape = (ntrials, nthreads)
 
-tmean = array([times[:, i].mean() for i in range(nthreads)])
-tstd = array([times[:, i].std() for i in range(nthreads)])
-terr = tstd/sqrt(ntrials)
+    tmean = array([times[:, i].mean() for i in range(nthreads)])
+    tstd = array([times[:, i].std() for i in range(nthreads)])
+    terr = tstd/sqrt(ntrials)
 
-E = array([tmean[0]/((i+1)*tmean[i]) for i in range(nthreads)])
-idealE = array([1 for i in range(nthreads)])
+    E = array([tmean[0]/((i+1)*tmean[i]) for i in range(nthreads)])
+    idealE = array([1 for i in range(nthreads)])
 
-S = array([tmean[0]/tmean[i] for i in range(nthreads)])
-idealS = array([i+1 for i in range(nthreads)])
+    S = array([tmean[0]/tmean[i] for i in range(nthreads)])
+    idealS = array([i+1 for i in range(nthreads)])
 
-print "Percentage errors: ", 100*terr/tmean
+    print "Percentage errors: ", 100*terr/tmean
 
-# Standard error for both efficiency and speedup
-error = array([(sqrt((tstd[i]/tmean[i])**2 + (tstd[0]/tmean[0])**2)/E[i])/sqrt(ntrials) for i in range(nthreads)])
+    # Standard error for both efficiency and speedup
+    error = array([(sqrt((tstd[i]/tmean[i])**2 + (tstd[0]/tmean[0])**2)/E[i])/sqrt(ntrials) for i in range(nthreads)])
 
-thread = array([i+1 for i in range(nthreads)])
+    thread = array([i+1 for i in range(nthreads)])
 
-pylab.errorbar(thread, tmean, yerr=terr, label=benchmark)
+    pylab.figure(0)
+    pylab.errorbar(thread, tmean, yerr=terr, label=filename)
+
+    pylab.figure(1)
+    pylab.errorbar(thread, E, yerr=error, label=filename)
+    
+    pylab.figure(2)
+    pylab.errorbar(thread, S, yerr=error, label=filename)
+
+pylab.figure(0)
 pylab.legend(loc="best")
 pylab.xlabel("Number of threads")
 pylab.ylabel("Time (Seconds)")
-pylab.savefig(benchmark+"_time.pdf")
+pylab.savefig("times.pdf")
 
+pylab.figure(1)
 pylab.plot(thread, idealE, label="Ideal")
-pylab.errorbar(thread, E, yerr=error, label=benchmark)
 pylab.legend(loc="best")
 pylab.xlabel("Number of threads")
 pylab.ylabel("Parallel efficiency")
-pylab.savefig(benchmark+"_efficiency.pdf")
+pylab.savefig("parallel_efficiency.pdf")
 
+pylab.figure(2)
 pylab.plot(thread, idealS, label="Ideal")
-pylab.errorbar(thread, S, yerr=error, label=benchmark)
 pylab.legend(loc="best")
 pylab.xlabel("Number of threads")
 pylab.ylabel("Speedup")
-pylab.savefig(benchmark+"_speedup.pdf")
+pylab.savefig("speedup.pdf")
+pylab.clf()
+
