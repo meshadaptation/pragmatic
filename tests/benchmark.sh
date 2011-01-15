580e1505dbeef2f19588190a26d5823e77435128
ggorman <>
2011-01-15 11:46:30 +0000

    Let output go to stderr/stdout rather than redirecting to a file. Let the call take care of that.

--- tests/benchmark.sh
+++ tests/benchmark.sh
@@ -5,14 +5,11 @@ input=$2
 ntrials=$3
 max_threads=$4
 
-output=${benchmark}.log
-cat /dev/null > ${output}
-
 for((n=0;n<ntrials;n++))
 do
     for((i=1;i<=max_threads;i++))
     do
         echo "Running trial $n on $i threads."
-        OMP_NUM_THREADS=$i ${benchmark} ${input} &>> ${output}
+        OMP_NUM_THREADS=$i ${benchmark} ${input}
     done
 done
