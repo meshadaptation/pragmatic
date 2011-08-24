e55e2ee538be751519abc941357cde99d8f9a4fc
Gerard Gorman <g.gorman@imperial.ac.uk>
2011-08-24 23:43:33 +0100

    Corrected CGAL support trap.

--- tests/src/test_hessian_2d.cpp
+++ tests/src/test_hessian_2d.cpp
@@ -26,6 +26,8 @@
  *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  *    USA
  */
+#include "pragmatic_config.h"
+
 #ifndef HAVE_LIBCGAL
 #warning No CGAL support. Cannot run test suite.
 int main(){
