2f73061136734c43a1bbf9caa4bbecfb0f8b59fd
Gerard Gorman <g.gorman@imperial.ac.uk>
2012-04-01 22:38:32 +0100

    Removed dependency on CGAL

--- tests/src/test_hessian_2d.cpp
+++ tests/src/test_hessian_2d.cpp
@@ -28,13 +28,6 @@
  */
 #include "pragmatic_config.h"
 
-#ifndef HAVE_LIBCGAL
-#warning No CGAL support. Cannot run test suite.
-int main(){
-    return 0;
-}
-#else
-
 #include <iostream>
 #include <vector>
 
@@ -46,76 +39,25 @@ int main(){
 #include "MetricField.h"
 #include "ticker.h"
 
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/Constrained_triangulation_plus_2.h> 
-#include <CGAL/Delaunay_mesher_2.h>
-#include <CGAL/Delaunay_mesh_face_base_2.h>
-#include <CGAL/Delaunay_mesh_size_criteria_2.h>
-#include <CGAL/Triangulation_conformer_2.h>
-
-struct K : public CGAL::Exact_predicates_inexact_constructions_kernel {};
-typedef CGAL::Triangulation_vertex_base_2<K> Vb;
-typedef CGAL::Delaunay_mesh_face_base_2<K> Fb;
-typedef CGAL::Triangulation_data_structure_2<Vb, Fb> Tds;
-typedef CGAL::Constrained_Delaunay_triangulation_2<K, Tds, CGAL::Exact_predicates_tag> CDT;
-typedef CGAL::Delaunay_mesh_size_criteria_2<CDT> Criteria;
-typedef CGAL::Delaunay_mesher_2<CDT, Criteria> Mesher;
-
-typedef CDT::Point Point;
-typedef CDT::Vertex_handle Vertex_handle;
+#include <mpi.h>
 
 using namespace std;
 
 int main(int argc, char **argv){
-  CDT cdt;
-  double xmin=-1.0, xmax=1.0, ymin=-1.0, ymax=1.0;
-  
-  // Define points - corners of bounding box.
-  deque<Vertex_handle> cdt_pts(4);
-  cdt_pts[0] = cdt.insert(Point(xmin, ymin));
-  cdt_pts[1] = cdt.insert(Point(xmax, ymin));
-  cdt_pts[2] = cdt.insert(Point(xmax, ymax));
-  cdt_pts[3] = cdt.insert(Point(xmin, ymax));
+  MPI::Init(argc,argv);
 
-  // Define edges
-  int edges[] = {0,1, 1,2, 2,3, 3,0};
-  for(size_t i=0;i<4;i++)
-    cdt.insert_constraint(cdt_pts[edges[i*2]], cdt_pts[edges[i*2+1]]);
+  Mesh<double, int> *mesh=VTKTools<double, int>::import_vtu("../data/box200x200.vtu");
 
-  // Refine mesh
-  double shape_bound = 0.125; // Shape bound. It corresponds to about 20.6 degree.
-  double max_edge_length=(xmax-xmin)/100;
-  CGAL::refine_Delaunay_mesh_2(cdt, Criteria(shape_bound, max_edge_length));
+  Surface<double, int> surface(*mesh, true);
 
-  // Export points
-  vector<double> x, y;
-  map<Point, size_t> coordToId;
-  size_t index = 0;
-  for(CDT::Point_iterator it=cdt.points_begin();it!=cdt.points_end();it++, index++){
-    x.push_back(CGAL::to_double(it->x()));
-    y.push_back(CGAL::to_double(it->y()));
-    coordToId[*it] = index;
-  }
-  int NNodes = x.size();
-
-  // Export edges
-  vector<int> enlist;
-  for(CDT::Finite_faces_iterator it=cdt.faces_begin();it!=cdt.faces_end();it++){
-    enlist.push_back(coordToId[it->vertex(0)->point()]);
-    enlist.push_back(coordToId[it->vertex(1)->point()]);
-    enlist.push_back(coordToId[it->vertex(2)->point()]);
-  }
-  size_t NElements = enlist.size()/3;
-  Mesh<double, int> *mesh= new Mesh<double, int>(NNodes, NElements, &(enlist[0]), &(x[0]), &(y[0]));
-
-  Surface<double, int> surface(*mesh);
+  size_t NNodes = mesh->get_number_nodes();
 
   // Set up field - use first touch policy
   vector<double> psi(NNodes);
 #pragma omp parallel
   {
 #pragma omp for schedule(static)
-    for(int i=0;i<NNodes;i++)
+    for(size_t i=0;i<NNodes;i++)
       psi[i] = pow(mesh->get_coords(i)[0]+0.1, 2) + pow(mesh->get_coords(i)[1]+0.1, 2);
   }
   
@@ -136,7 +78,7 @@ int main(int argc, char **argv){
     metric_field.get_metric(&(metric[0]));
     
     double rms[] = {0., 0., 0., 0.};
-    for(int i=0;i<NNodes;i++){
+    for(size_t i=0;i<NNodes;i++){
       rms[0] += pow(2.0-metric[i*4  ], 2); rms[1] += pow(    metric[i*4+1], 2);
       rms[2] += pow(    metric[i*4+2], 2); rms[3] += pow(2.0-metric[i*4+3], 2);
     }
@@ -163,5 +105,4 @@ int main(int argc, char **argv){
   
   return 0;
 }
-#endif
 
