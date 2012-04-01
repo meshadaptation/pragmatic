5d9c28ead9f80852ee76dfe7b442107222210d32
Gerard Gorman <g.gorman@imperial.ac.uk>
2012-04-01 22:30:50 +0100

    Tweaks to benchmarking script

--- tests/benchmark-analysis.py
+++ tests/benchmark-analysis.py
@@ -7,22 +7,15 @@ import sys
 import getopt
 
 # Defaults
-files = []
-column = 0
-ntrials = 1
+ntrials = 10
 key=""
 thread_cnt=[1, 2, 4, 6, 8, 10, 12]
 
-opts, args = getopt.getopt(sys.argv[1:], 'p:t:b:')
+opts, args = getopt.getopt(sys.argv[1:], 'p:')
 
 for o, a in opts:
     if o == '-p':
-      key, ocol = a.split(":")
-      column = int(ocol)
-    elif o == '-t':
-        ntrials = int(a)
-    elif o == '-b':
-        benchmark = a
+      key = a
 
 filename = args[0]
 basename = filename[:-4]
@@ -32,7 +25,7 @@ t = [[], [], []]
 pos=0
 for line in file:
     if line.count(key):
-        t[pos] = t[pos] + [float(line.split()[column])]
+        t[pos] = t[pos] + [float(line.split()[-1])]
         pos = (pos+1)%3
 
 times = [array(t[0]), array(t[1]), array(t[2])]
@@ -45,39 +38,29 @@ tmean = [array([times[0][:, i].mean() for i in range(nthreads)]),
          array([times[1][:, i].mean() for i in range(nthreads)]),
          array([times[2][:, i].mean() for i in range(nthreads)])]
          
-tstd = [array([times[0][:, i].std() for i in range(nthreads)]),
-        array([times[1][:, i].std() for i in range(nthreads)]),
-        array([times[2][:, i].std() for i in range(nthreads)])]
+terr = [array([times[0][:, i].std()/sqrt(ntrials) for i in range(nthreads)]),
+        array([times[1][:, i].std()/sqrt(ntrials) for i in range(nthreads)]),
+        array([times[2][:, i].std()/sqrt(ntrials) for i in range(nthreads)])]
 
-terr = [tstd[0]/sqrt(ntrials),
-        tstd[1]/sqrt(ntrials),
-        tstd[2]/sqrt(ntrials)]
+E = [array([(times[0][:, 0]/(thread_cnt[i]*times[0][:, i])).mean() for i in range(nthreads)]),
+     array([(times[1][:, 0]/(thread_cnt[i]*times[1][:, i])).mean() for i in range(nthreads)]),
+     array([(times[2][:, 0]/(thread_cnt[i]*times[2][:, i])).mean() for i in range(nthreads)])]
 
-E = [array([tmean[0][0]/((i+1)*tmean[0][i]) for i in range(nthreads)]),
-     array([tmean[1][0]/((i+1)*tmean[1][i]) for i in range(nthreads)]),
-     array([tmean[2][0]/((i+1)*tmean[2][i]) for i in range(nthreads)])]
-idealE = array([1 for i in range(nthreads)])
+Eerr = [array([(times[0][:, 0]/(thread_cnt[i]*times[0][:, i])).std()/sqrt(ntrials) for i in range(nthreads)]),
+        array([(times[1][:, 0]/(thread_cnt[i]*times[1][:, i])).std()/sqrt(ntrials) for i in range(nthreads)]),
+        array([(times[2][:, 0]/(thread_cnt[i]*times[2][:, i])).std()/sqrt(ntrials) for i in range(nthreads)])]
 
-S = [array([tmean[0][0]/tmean[0][i] for i in range(nthreads)]),
-     array([tmean[1][0]/tmean[1][i] for i in range(nthreads)]),
-     array([tmean[2][0]/tmean[2][i] for i in range(nthreads)])]
+
+S = [array([(times[0][:, 0]/times[0][:, i]).mean() for i in range(nthreads)]),
+     array([(times[1][:, 0]/times[1][:, i]).mean() for i in range(nthreads)]),
+     array([(times[2][:, 0]/times[2][:, i]).mean() for i in range(nthreads)])]
+
+Serr = [array([(times[0][:, 0]/times[0][:, i]).std()/sqrt(ntrials) for i in range(nthreads)]),
+        array([(times[1][:, 0]/times[1][:, i]).std()/sqrt(ntrials) for i in range(nthreads)]),
+        array([(times[2][:, 0]/times[2][:, i]).std()/sqrt(ntrials) for i in range(nthreads)])]
+
 idealS = array(thread_cnt)
-
-print "Percentage errors: ", 100*terr[0]/tmean[0], 100*terr[1]/tmean[1], 100*terr[2]/tmean[2]
-
-# Standard error for both efficiency and speedup
-error = [array([(sqrt((tstd[0][i]/tmean[0][i])**2 + (tstd[0][0]/tmean[0][0])**2)/E[0][i])/sqrt(ntrials)
-                for i in range(nthreads)]),
-         array([(sqrt((tstd[1][i]/tmean[1][i])**2 + (tstd[1][0]/tmean[1][0])**2)/E[1][i])/sqrt(ntrials)
-                for i in range(nthreads)]),
-         array([(sqrt((tstd[2][i]/tmean[2][i])**2 + (tstd[2][0]/tmean[2][0])**2)/E[2][i])/sqrt(ntrials)
-                for i in range(nthreads)])]
-
 thread = array(thread_cnt)
-
-print "tmean[0] = ", tmean[0], terr[0]
-print "tmean[1] = ", tmean[1], terr[1]
-print "tmean[2] = ", tmean[2], terr[2]
 
 pylab.figure(0)
 pylab.errorbar(thread, tmean[0], yerr=terr[0], label="default")
@@ -89,20 +72,18 @@ pylab.ylabel("Time (Seconds)")
 pylab.savefig(basename+"-times.pdf")
 
 pylab.figure(1)
-pylab.errorbar(thread, E[0], yerr=error[0], label="default")
-pylab.errorbar(thread, E[1], yerr=error[1], label="custom")
-pylab.errorbar(thread, E[2], yerr=error[2], label="scatter")
+pylab.errorbar(thread, E[0], yerr=Eerr[0], label="default")
+pylab.errorbar(thread, E[1], yerr=Eerr[1], label="custom")
+pylab.errorbar(thread, E[2], yerr=Eerr[2], label="scatter")
 pylab.legend(loc="best")
-pylab.plot(thread, idealE, label="Ideal")
-pylab.legend(loc=4)
 pylab.xlabel("Number of threads")
 pylab.ylabel("Parallel efficiency")
 pylab.savefig(basename+"-parallel_efficiency.pdf")
 
 pylab.figure(2)
-pylab.errorbar(thread, S[0], yerr=error[0], label="default")
-pylab.errorbar(thread, S[1], yerr=error[1], label="custom")
-pylab.errorbar(thread, S[2], yerr=error[2], label="scatter")
+pylab.errorbar(thread, S[0], yerr=Serr[0], label="default")
+pylab.errorbar(thread, S[1], yerr=Serr[1], label="custom")
+pylab.errorbar(thread, S[2], yerr=Serr[2], label="scatter")
 pylab.legend(loc="best")
 pylab.plot(thread, idealS, label="Ideal")
 pylab.xlabel("Number of threads")
