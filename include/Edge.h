d45880c99a4585b934676cc2fc0ff1f56540156e
Gerard Gorman <g.gorman@imperial.ac.uk>
2012-06-13 20:12:00 +0100

    Add directed edge

--- include/Edge.h
+++ include/Edge.h
@@ -100,5 +100,75 @@ template<typename index_t> class Edge{
   std::pair<index_t, index_t> edge;
 };
 
+template<typename index_t> class DirectedEdge{
+ public:
+  /*! Constructor
+   * @param nid0 Node-Id 0
+   * @param nid1 Node-Id 1
+   */
+  DirectedEdge(index_t nid0, index_t nid1){
+    edge.first = nid0;
+    edge.second = nid1;
+  }
+  
+  /*! Copy constructor
+   * @param in DirectedEdge object.
+   */
+  DirectedEdge(const DirectedEdge& in){
+    *this = in;
+  }
+
+  // Default constructor.
+  DirectedEdge(){}
+
+  /// Destructor
+  ~DirectedEdge(){}
+  
+  /// Assignment operator
+  DirectedEdge& operator=(const DirectedEdge &in){
+    edge = in.edge;
+    id = in.id;
+
+    return *this;
+  }
+  
+  /// Equality operator.
+  bool operator==(const DirectedEdge& in) const{
+    return this->edge == in.edge;
+  }
+
+  /// Inequality operator.
+  bool operator!=(const DirectedEdge& in) const{
+    return this->edge != in.edge;
+  }
+
+  /// Less-than operator
+  bool operator<(const DirectedEdge& in) const{
+    return this->edge < in.edge;
+  }
+
+  index_t connected(const DirectedEdge& in) const{
+    if((edge.first==in.edge.first)||(edge.first==in.edge.second))
+      return edge.first;
+    else if((edge.second==in.edge.first)||(edge.second==in.edge.second))
+      return edge.second;
+    return -1;
+  }
+  
+  bool contains(index_t nid) const{
+    return (nid==edge.first)||(nid==edge.second);
+  }
+
+  template<typename _real_t, typename _index_t> friend class Mesh;
+  template<typename _real_t, typename _index_t> friend class Surface;
+  template<typename _real_t, typename _index_t> friend class Coarsen;
+  template<typename _real_t, typename _index_t> friend class Swapping;
+  template<typename _real_t, typename _index_t> friend class Refine;
+
+ private:
+  index_t id;
+  std::pair<index_t, index_t> edge;
+};
+
 #endif
 
