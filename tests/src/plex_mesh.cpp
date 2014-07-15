10cff164ef82181ac3d883ffb3d7cc08f92238ba
Gerard Gorman <g.gorman@imperial.ac.uk>
2014-07-15 16:33:53 +0100

    Start of DMPlex support.

---
+++ tests/src/plex_mesh.cpp
@@ -0,0 +1,107 @@
+#include <cfloat>
+#include <cassert>
+
+#include "plex_mesh.h"
+
+PetscErrorCode create_unit_square(int I, int J, MPI_Comm comm, DM *mesh){
+  DM boundary;
+  PetscErrorCode ierr = DMPlexCreate(comm, &boundary); CHKERRQ(ierr);
+  ierr = DMSetType(boundary, DMPLEX); CHKERRQ(ierr);
+  ierr = DMPlexSetDimension(boundary, 1); CHKERRQ(ierr);
+  
+  PetscReal lower[] = {0, 0};
+  PetscReal upper[] = {1, 1};
+  PetscInt edges[] = {I, J};
+  ierr = DMPlexCreateSquareBoundary(boundary, lower, upper, edges); CHKERRQ(ierr);
+  
+  DM lmesh;
+  ierr = DMPlexGenerate(boundary, NULL, PETSC_TRUE, &lmesh); CHKERRQ(ierr);
+
+  // Apply boundary IDs
+  ierr = DMPlexCreateLabel(lmesh, "boundary_ids"); CHKERRQ(ierr);
+  
+  DMLabel label;
+  ierr = DMPlexGetLabel(lmesh, "boundary_ids", &label); CHKERRQ(ierr);
+
+  ierr = DMPlexMarkBoundaryFaces(lmesh, label); CHKERRQ(ierr);
+
+  Vec coords;
+  ierr = DMGetCoordinates(lmesh, &coords); CHKERRQ(ierr);
+
+  PetscSection coord_sec;
+  ierr = DMGetCoordinateSection(lmesh, &coord_sec); CHKERRQ(ierr);
+
+  PetscInt size;
+  ierr = DMPlexGetStratumSize(lmesh, "boundary_faces", 1, &size); CHKERRQ(ierr);
+
+  assert(size>0);
+
+  IS regionIS;
+  ierr = DMPlexGetStratumIS(lmesh, "boundary_faces", 1, &regionIS); CHKERRQ(ierr);
+
+  const PetscInt *boundary_faces;
+  ISGetIndices(regionIS, &boundary_faces);
+
+  for(int i=0;i<size;i++){
+    PetscInt face = boundary_faces[i];
+
+    PetscInt csize;
+    PetscScalar *face_coords;
+    ierr = DMPlexVecGetClosure(lmesh, coord_sec, coords, face, &csize, &face_coords); CHKERRQ(ierr);
+
+    assert(csize==4);
+
+    if(fabs(face_coords[0])<DBL_EPSILON && fabs(face_coords[2])<DBL_EPSILON){
+      DMPlexSetLabelValue(lmesh, "boundary_ids", face, 1);
+    }else if(fabs(face_coords[0]-1.0)<DBL_EPSILON && fabs(face_coords[2]-1.0)<DBL_EPSILON){
+      DMPlexSetLabelValue(lmesh, "boundary_ids", face, 2);
+    }else if(fabs(face_coords[1])<DBL_EPSILON && fabs(face_coords[3])<DBL_EPSILON){
+      DMPlexSetLabelValue(lmesh, "boundary_ids", face, 3);
+    }else if(fabs(face_coords[1]-1.0)<DBL_EPSILON && fabs(face_coords[3]-1.0)<DBL_EPSILON){
+      DMPlexSetLabelValue(lmesh, "boundary_ids", face, 4);
+    }
+ }
+
+  int nranks;
+  MPI_Comm_size(comm, &nranks);
+  if(nranks>1){
+    ierr = DMPlexDistribute(lmesh, NULL, 1, NULL, mesh);
+  }else{
+     *mesh = lmesh;
+  }
+
+  ierr = DMDestroy(&boundary); CHKERRQ(ierr);
+
+  return ierr;
+}
+/*
+DM create_unit_cube(int I, int J, size_t K, MPI_Comm comm){
+  DM boundary;
+  PetscErrorCode ierr = DMPlexCreate(comm, &boundary); CHKERRQ(ierr);
+  ierr = DMSetType(boundary, DMPLEX); CHKERRQ(ierr);
+  ierr = DMPlexSetDimension(boundary, 2); CHKERRQ(ierr);
+  
+  PetscReal lower[] = {0, 0, 0};
+  PetscReal upper[] = {1, 1, 1};
+  PetscInt edges[] = {I, J, K};
+  ierr = DMPlexCreateCubeBoundary(boundary, lower, upper, edges); CHKERRQ(ierr);
+  
+  DM mesh;
+  ierr = DMPlexGenerate(boundary, NULL, PETSC_TRUE, &mesh); CHKERRQ(ierr);
+  
+  ierr = DMDestroy(&boundary); CHKERRQ(ierr);
+
+  int nranks;
+  MPI_Comm_size(comm, &nranks);
+  if(nranks>1){
+    DM pmesh;
+
+    ierr = DMPlexDistribute(mesh, NULL, 1, NULL, &pmesh);
+    // Need to check with valgrind if I need to destroy mesh
+
+    mesh = pmesh;
+  }
+
+  return mesh;
+}
+*/
---
+++ tests/src/test_plex_mesh.cpp
@@ -0,0 +1,28 @@
+#include "plex_mesh.h"
+
+int main(int argv, char **argc){
+  PetscErrorCode ierr = PetscInitialize(&argv, &argc, NULL, "spoon feeding"); CHKERRQ(ierr);
+  
+  DM unit_square_mesh;
+  ierr = create_unit_square(10, 10, MPI_COMM_WORLD, &unit_square_mesh); CHKERRQ(ierr);
+  ierr = DMView(unit_square_mesh, PETSC_VIEWER_STDOUT_WORLD); CHKERRQ(ierr);
+
+  /*
+  int NVerts, NElements;
+  ///....
+
+  std::vector<double> x(NVerts), y(NVerts);
+
+  std::vector<int> ENList(NElements*3); // {n0, n1, n2,  ...
+  std::vector<int> boundary(NElements*3); // {b0, b1, b2,  ...
+
+
+  DM cube_mesh = create_unit_cube(10, 10, 10);
+  ierr = DMView(square_mesh, PETSC_VIEWER_STDOUT_WORLD); CHKERRQ(ierr);
+  */
+
+  ierr = PetscFinalize(); CHKERRQ(ierr);
+
+  return 0;
+}
+
