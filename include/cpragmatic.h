75c28dcc05a015f32ecae9bad12b4901c6553882
Patrick Farrell <patrick.farrell@maths.ox.ac.uk>
2016-05-10 08:44:45 +0100

    Make pragmatic_adapt take in the coarsen_surface flag.

--- include/cpragmatic.h
+++ include/cpragmatic.h
@@ -9,7 +9,7 @@ void pragmatic_3d_init(const int *NNodes, const int *NElements, const int *enlis
 void pragmatic_set_boundary(const int *nfacets, const int *facets, const int *ids);
 void pragmatic_set_metric(const double *metric);
 void pragmatic_add_field(const double *psi, const double *error, int *pnorm);
-void pragmatic_adapt(void);
+void pragmatic_adapt(int coarsen_surface);
 void pragmatic_get_info(int *NNodes, int *NElements);
 void pragmatic_get_coords_2d(double *x, double *y);
 void pragmatic_get_coords_3d(double *x, double *y, double *z);
--- src/cpragmatic.cpp
+++ src/cpragmatic.cpp
@@ -201,7 +201,7 @@ extern "C" {
 
     /** Adapt the mesh.
     */
-    void pragmatic_adapt()
+    void pragmatic_adapt(int coarsen_surface)
     {
         Mesh<double> *mesh = (Mesh<double> *)_pragmatic_mesh;
 
@@ -223,7 +223,7 @@ extern "C" {
             for(size_t i=0; i<20; i++) {
                 double L_ref = std::max(alpha*L_max, L_up);
 
-                coarsen.coarsen(L_low, L_ref);
+                coarsen.coarsen(L_low, L_ref, (bool) coarsen_surface);
                 swapping.swap(0.7);
                 refine.refine(L_ref);
 
@@ -243,7 +243,7 @@ extern "C" {
             Refine<double, 3> refine(*mesh);
             Swapping<double, 3> swapping(*mesh);
 
-            coarsen.coarsen(L_low, L_up);
+            coarsen.coarsen(L_low, L_up, (bool) coarsen_surface);
 
             double L_max = mesh->maximal_edge_length();
 
@@ -252,7 +252,7 @@ extern "C" {
                 double L_ref = std::max(alpha*L_max, L_up);
 
                 refine.refine(L_ref);
-                coarsen.coarsen(L_low, L_ref);
+                coarsen.coarsen(L_low, L_ref, (bool) coarsen_surface);
                 swapping.swap(0.95);
 
                 L_max = mesh->maximal_edge_length();
