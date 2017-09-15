a26fc0a30772028eb14793389b9d39a9d299360a
Nicolas Barral <n.barral@imperial.ac.uk>
2017-09-15 10:51:46 +0100

    update egads test + fix cmake

--- tests/src/test_egads.cpp
+++ tests/src/test_egads.cpp
@@ -1,32 +1,35 @@
 #include "Mesh.h"
 
-#include "ticker.h"
+#ifdef HAVE_LIBMESHB
+#include "GMFTools.h"
+#endif
 
+#include "MetricField.h"
+#include "cpragmatic.h"
 
 int main(int argc, char **argv)
 {
 
-#ifdef HAVE_MPI
     int required_thread_support=MPI_THREAD_SINGLE;
     int provided_thread_support;
     MPI_Init_thread(&argc, &argv, required_thread_support, &provided_thread_support);
     assert(required_thread_support==provided_thread_support);
-#endif
 
-    int tri[3] = {0, 1, 2};
-    double x[3] = {0, 0, 1}, y[3] = {0, 1, 0};
-    Mesh<double> mesh = Mesh<double>(3, 1, tri, x, y);
+    char filename_in[256];
+    sprintf(filename_in, "../data/cube-cylinder");
+
+    Mesh<double> *mesh=GMFTools<double>::import_gmf_mesh(filename_in);
+    pragmatic_init_light((void*)mesh);
+//    MetricField<double,3> *metric = GMFTools<double>::import_gmf_metric3d(filename_in, *mesh);
+//    metric->update_mesh();
 #ifdef HAVE_EGADS
-    int res = mesh.analyzeCAD("../data/cube-cylinder.step");
+    int res = mesh->analyzeCAD("../data/cube-cylinder.step");
     if (!res) printf("pass\n");
 
-    mesh.associate_CAD_with_Mesh();
+    mesh->associate_CAD_with_Mesh();
     printf("pass\n");
 #endif
 
-#ifdef HAVE_MPI
     MPI_Finalize();
-#endif
-
     return 0;
 }
