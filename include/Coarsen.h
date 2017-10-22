6ce70041e8f23620699414549d0d6f2a2e4e1d20
Nicolas Barral <n.barral@imperial.ac.uk>
2017-10-22 19:43:07 +0100

    fix previous

--- include/Coarsen.h
+++ include/Coarsen.h
@@ -502,7 +502,8 @@ private:
         if(reject_collapse)
             return -2;
 
-        if (tag) printf("DEBUG  collapse accepted onto vertex: %d (%1.3f %1.3f %1.3f)\n", target_vertex, _mesh->_coords[3*target_vertex],
+        if (tag && target_vertex>0) 
+            printf("DEBUG  collapse accepted onto vertex: %d (%1.3f %1.3f %1.3f)\n", target_vertex, _mesh->_coords[3*target_vertex],
                         _mesh->_coords[3*target_vertex+1], _mesh->_coords[3*target_vertex+2]);
 
         return target_vertex;
@@ -597,8 +598,50 @@ private:
             _mesh->NEList[target_vertex].insert(eid);
         }
 
+        // Update surface edges - have to do it here as it is based on NNList
+        if (_mesh->node_topology[rm_vertex].size()>0 && _mesh->node_topology[target_vertex].size()>0) {
+            // TODO SOMEWHERE, I NEED TO MAKE SURE I AM NOT BREAKING MY SURFACE
+            for(const auto& nid : _mesh->NNList[rm_vertex]) {
+                if (_mesh->node_topology[nid].size()>0) {
+                    int tag_add_surface_edge = 0;
+                    if (nid < rm_vertex) {
+                        auto it = std::find(_mesh->NNList_surface[nid].begin(), _mesh->NNList_surface[nid].end(), rm_vertex);
+                        if (it != _mesh->NNList_surface[nid].end()) {
+                            _mesh->NNList_surface[nid].erase(it);
+                            tag_add_surface_edge = 1;
+                        }
+                    }
+                    else {
+                        auto it = std::find(_mesh->NNList_surface[rm_vertex].begin(), _mesh->NNList_surface[rm_vertex].end(), nid);
+                        if (it != _mesh->NNList_surface[rm_vertex].end()) {
+                            tag_add_surface_edge = 1;
+                        }
+                    }
+                    if (tag_add_surface_edge) { // I want to add nid-target_vertex or target_vertex-nid
+                        if (nid < target_vertex) {
+                            auto it = std::find(_mesh->NNList_surface[nid].begin(), _mesh->NNList_surface[nid].end(), target_vertex);
+                            if (it == _mesh->NNList_surface[nid].end()) {
+                                _mesh->NNList_surface[nid].push_back(target_vertex);
+                                //if (nid==31894 && target_vertex==44473) printf("DEBUG  VOILA\n");
+                            }
+                        }
+                        else {
+                            auto it = std::find(_mesh->NNList_surface[target_vertex].begin(), _mesh->NNList_surface[target_vertex].end(), nid);
+                            if (it == _mesh->NNList_surface[target_vertex].end()) {
+                                _mesh->NNList_surface[target_vertex].push_back(nid);
+                                //if (target_vertex==31894 && nid==44473) 
+                                //    printf("DEBUG  VOICI, rm_vertex: %d (%1.2f %1.2f %1.2f), target_vertex: %d (%1.2f %1.2f %1.2f), nid: %d (%1.2f %1.2f %1.2f)\n",
+                                //        rm_vertex, _mesh->_coords[3*rm_vertex], _mesh->_coords[3*rm_vertex+1], _mesh->_coords[3*rm_vertex+2],
+                                //        target_vertex, _mesh->_coords[3*target_vertex], _mesh->_coords[3*target_vertex+1], _mesh->_coords[3*target_vertex+2],
+                                //        nid, _mesh->_coords[3*nid], _mesh->_coords[3*nid+1], _mesh->_coords[3*nid+2]);
+                            }
+                        }
+                    }
+                }
+            }                        
+        }
+
         // Update surrounding NNList.
-        // TODO HERE, we do not erase NNList[rm_vertex] and NEList[rm_vertex]: should we ?
         for(const auto& nid : _mesh->NNList[rm_vertex]) {
             auto it = std::find(_mesh->NNList[nid].begin(), _mesh->NNList[nid].end(), rm_vertex);
             _mesh->NNList[nid].erase(it);
@@ -609,30 +652,6 @@ private:
 
             if(std::find(_mesh->NNList[target_vertex].begin(), _mesh->NNList[target_vertex].end(), nid)==_mesh->NNList[target_vertex].end())
                 _mesh->NNList[target_vertex].push_back(nid);
-        }
-
-        if (_mesh->node_topology[rm_vertex].size()>0 && _mesh->node_topology[target_vertex].size()>0) {
-            for(const auto& nid : _mesh->NNList_surface[rm_vertex]) {
-                if (nid < rm_vertex) {
-                    auto it = std::find(_mesh->NNList_surface[nid].begin(), _mesh->NNList_surface[nid].end(), rm_vertex);
-                    assert(it != _mesh->NNList_surface[nid].end());
-                    _mesh->NNList_surface[nid].erase(it);
-                }
-            }
-            for(const auto &nid : new_edges) {
-                if (_mesh->node_topology[nid].size()>0){
-                    if (nid < target_vertex) {
-                        auto it = std::find(_mesh->NNList_surface[nid].begin(), _mesh->NNList_surface[nid].end(), target_vertex);
-                        if (it == _mesh->NNList_surface[nid].end())
-                            _mesh->NNList_surface[nid].push_back(target_vertex);
-                    }
-                    else {
-                        auto it = std::find(_mesh->NNList_surface[target_vertex].begin(), _mesh->NNList_surface[target_vertex].end(), nid);
-                        if (it == _mesh->NNList_surface[target_vertex].end())
-                            _mesh->NNList_surface[target_vertex].push_back(nid);
-                    }        
-                }            
-            }
         }
 
         _mesh->erase_vertex(rm_vertex);
