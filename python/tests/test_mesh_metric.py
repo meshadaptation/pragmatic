799c159f6f1a369dffb721dbccc2dafeea743c2e
croci <matteo.croci@maths.ox.ac.uk>
2017-11-09 18:10:03 +0000

    added check for exact metric in test_mesh_metric.py, changed mesh_metric routine by using the exact formula in George and Borouchaki paper

--- python/tests/test_mesh_metric.py
+++ python/tests/test_mesh_metric.py
@@ -3,8 +3,10 @@ import os
 sys.path.append(os.path.abspath('..'))
 from dolfin import *
 from adaptivity import *
+from numpy import array
 
 check = []
+test_metric = array([[1.0, -0.5], [-0.5, 1.0]])
 
 for n in range(2, 20):
     mesh = UnitSquareMesh(mpi_comm_world(), n,n)
@@ -17,11 +19,14 @@ for n in range(2, 20):
     if abs(A - A[0,:,:]).max()/A.max() > 1.0e-14:
         raise RuntimeError('Test failed: the metric is not constant on each element')
 
-    a = A[0,:,:]
+    a = A[0,:,:]*mesh.hmax()**2/2.
 
-    check.append(a.max()*mesh.hmax()**2)
+    if abs(a-test_metric).max() > 1.0e-14:
+        raise RuntimeError('Test failed: the metric is not correct')
 
-# the metric should scale like O(1/h^2)
+    check.append(a.max())
+
+# the metric should scale like O(1/h^2). This test is redundant because we are checking whether the metric is exact.
 if max(check) - min(check) > 1.0e-14:
     raise RuntimeError('Test failed: the metric does not scale like O(1/h^2)')
 
