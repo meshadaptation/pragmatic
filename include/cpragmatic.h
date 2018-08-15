7a6186ce3737b2c978dfe3881b2f3d6e1cb8241c
Nicolas Barral <n.barral@imperial.ac.uk>
2018-08-15 11:13:42 +0100

    add proper declarations

--- include/cpragmatic.h
+++ include/cpragmatic.h
@@ -12,6 +12,8 @@ void pragmatic_init_light(void* mesh);
 void pragmatic_set_boundary(const int *nfacets, const int *facets, const int *ids);
 void pragmatic_set_metric(const double *metric);
 void pragmatic_add_field(const double *psi, const double *error, int *pnorm);
+void pragmatic_set_regions(const int *element_tags);
+void pragmatic_set_internal_boundaries();
 void pragmatic_adapt(int coarsen_surface, int coarsen_int_surface);
 void pragmatic_coarsen(int coarsen_surface);
 void pragmatic_get_info(int *NNodes, int *NElements);
